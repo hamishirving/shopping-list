13f1b8bdcb83e149245cfdf1e46b1bf58ce09129
Hamish Irving <hamish.workstuff@gmail.com>
2014-11-04 22:10:32 +0000

    Fix bugs

--- js/app.js
+++ js/app.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
 
-	function checkError() {
-		if ($('input').val().length === 0) {		// If input is empty - show error
-			$('.error').show();
-			return true;
-			} else {
-				$('.error').hide();					// Else - hide error
-				return false;
-				}
-	}
+	// Declare functions
 
-	function addItem() {
-		var item = $('input').val();
-		$('.wrap ').append('<div class="row"><div class="tick"></div><h3 class="item"></h3><p class="date"></p><div class="delete"></div></div>');
-		$('.row').show();
-		$('.item').eq( -1 ).append(item);
-		$('input').val('');
-	}
+	// getDate
 
 	function currentDate() {
 		var d = new Date();
@@ -25,64 +11,87 @@ $(document).ready(function() {
 		var fullDate = 
 			((''+day).length<2 ? '0' : '') + day + '-' +
 			((''+month).length<2 ? '0' : '') + month + '-' +
-			d.getFullYear();						// Get date in DD/MM/YYYY format
-		$('.date').eq( -1 ).append(fullDate);		// Append to date class
+			d.getFullYear();										// Get date in DD/MM/YYYY format
+		$('.date').eq(-1).append(fullDate);							// Append to date class
 	}
 
-	function rowInteract() {
-		
+	// addItem
+
+	function addItem() {
+		var item = $('input').val();
+		var row = $('<div class="row"><div class="tick"></div><h3 class="item"></h3><p class="date"></p><div class="delete"></div></div>');
+		$('.wrap').append(row);
+		$('.row').show();
+		$('.item').eq(-1).append(item);
+		$('input').val('');
+	}
+
+	// show enter icon
+
+	function showEnter() {
+		$('input').keyup(function () {
+			var len = $(this).val().length;
+			if (len > 0) {
+				$('form').find('.enter').css('display', 'block'); 	// Show enter icon after 1 character enetred
+			} else {
+				$('form').find('.enter').css('display', 'none');  	// Hide enter icon if no character entered
+			}
+		})
+	}
+
+	// show delete button
+
+	function showDelete() {
+
 		$('.row').on('mouseenter',(function() {
-			$(this).find('.delete').show('fast'); 	// Show Delete button on mouseenter
-		}));
+			$(this).find('.delete').show('fast');					// Show Delete button on mouseenter 						
+		}))
 
 		$('.row').on('mouseleave',(function() {
-			$('.delete').hide("fast"); 				// Hide delete button on mouseleave
+			$('.delete').hide(); 									// Hide delete button on mouseleave
 		}))
-
-		$('.row').on('click',(function() {
-			$(this).addClass('done');			// Toggle .done class when row clicked
-		}));
-
-		$('.row').not('.done').on('click',(function() {
-			$(this).find('.tick').show();			// Show/hide tick on click
-		}));
 
 		$('.delete').on('click',(function() {
-			$(this).parent('.row').remove();		// Delete row after delete clicked
+			$(this).parent('.row').slideUp('fast', function() {
+				$(this).remove();									// Delete row after delete clicked
+			})
 		}))
+	}
 
-		$('.done').on('click',(function() {
-			if ($(this).hasClass('.done')) {
-				alert('done clicked');
+	// row completed
+
+	function rowComplete() {
+
+		$('.row').on('click',(function() {							// Toggle .done class when row clicked
+			if ($(this).is('#done')) {
+				$(this).removeAttr('id');
 				$(this).find('.tick').hide();
-				$(this).removeClass('done');			// Toggle .done class when row clicked
-				return true;
+			} else {
+				$(this).attr('id', 'done');
+				$(this).find('.tick').show();
 			}
 		}));
+	}
 
-	}	
+	// On enter keydown, do the following
 
-	$('input').keyup(function () {
-		var len = $(this).val().length;
-		if (len > 0) {
-			$('form').find('.enter').css('display', 'block'); 	// Show enter icon after 1 character enetred
-		} else {
-			$('form').find('.enter').css('display', 'none');  // Hide enter icon if no character entered
-		}
-	})
-
-	$('input').keydown(function (enter) {
-		if (enter.keyCode == 13) {
-			checkError();
-			addItem();
-			currentDate();
-			rowInteract();
+	$('.wrap').on('keydown', 'input',(function (enter) {
+		if (enter.keyCode == 13) {									// If keydown is Enter key
+			if ($('input').val().length === 0) {					// If input is empty - show error
+				$('.error').show();
+				return false;
+			} else {												
+				$('.error').hide();									// Else hide error then add Item & Date
+				addItem();
+				currentDate();
+				showDelete();
+				rowComplete();
+				return false;
+			}
 			return false;
-		} 
-	});
+		}
+	}));
+
+	showEnter();
 
 })
-
-
-
-
